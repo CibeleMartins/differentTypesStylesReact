cd3f822e25414d7b2def9b5eb288f61f28cc6622
Cibele Martins <Cibele.Martins@ibm.com>
2022-09-30 12:17:21 -0300

    🚧 feat: Implementing component TableHomework

--- src/components/TableHomeworks/TableHomeworks.js
+++ src/components/TableHomeworks/TableHomeworks.js
@@ -3,12 +3,14 @@ import { useState } from 'react';
 import Button from '../Button/Button';
 import styled from 'styled-components';
 
+import {arrayHomeworks} from './data'
+
 
 const FormControl = styled.div `
 
     width: 500px;
     height: 400px;
-    margin-top: 200px;
+    margin-top: 100px;
     border-radius: 10px;
     background-color: grey;
     display: flex;
@@ -35,27 +37,72 @@ const InputDescription = styled.textarea `
     padding: 5px;
     flex-wrap: wrap;
 
-`
+`;
+
+
+const MainPostIt = styled.div `
+    margin-top: 40px;
+    width: 500px;
+    background-color: lavender;
+    border: 10px solid grey;
+    border-radius: 10px;
+    padding: 20px;
+    display: flex; 
+    flex-wrap: wrap;
+    justify-content: space-around;
+    aling-items:center;
+`;
+
+const PostIt = styled.div `
+
+    width: 200px;
+    height: 200px;
+    padding: 10px;
+    background-color: yellow
+    `;
 
 
 function TableHomewors () {
 
     const [homeworkValue, setHomework] = useState('')
+    const [description, setDescription] = useState('')
+ 
 
     const changeInputState = (event) => setHomework(event.target.value); 
+    const changeDescription = (event) => setDescription(event.target.value); 
+
+    const createPostIt = () => {
+
+        const homework = homeworkValue;
+        const descrip = description;
+        return  arrayHomeworks.push({title: homework, describe: descrip})
+   
+    }
 
     return (
+        <>
         <FormControl>
             <Input 
             value={homeworkValue} 
             placeholder='Homework'
             onChange={(event)=> changeInputState(event)}/>
             <InputDescription
-            value={homeworkValue} 
+            value={description} 
             placeholder='Description'
-            onChange={(event)=> changeInputState(event)}/>
-            <Button title="Insert"/>
+            onChange={(event)=> changeDescription(event)}/>
+            <Button onClick={()=> createPostIt()} title="Insert"/>
         </FormControl>
+
+        <MainPostIt>
+            {arrayHomeworks.map((h)=> {
+                return (<PostIt>
+                    <h3>{h.title}</h3>
+                    <p>{h.describe}</p>
+                </PostIt>)
+            })}
+        </MainPostIt>
+        </>
+
     )
 }
 
